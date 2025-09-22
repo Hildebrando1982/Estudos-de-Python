6a5cf31f58ff01f4716a04a092c22ad281b24ddf
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-22 20:43:24 -0300

    Create aula1.py
    Aqui você vai aprender a usar o Docstring (serve para fazer anotações que ocupa mais de uma linha - o compilador ignora este tipo de dado, serve apenas para o programador fazer suas anotrações).
    Já a hastags serve para comentários com uma linha -  o compilador ignora e não mostra na tela.
    Por último vimos o comando print, serve para exibir os dados na tela. Quando colocamos as palavras entre aspas significa que é um texto.

---
+++ aula1.py
@@ -0,0 +1,9 @@
+'''Chamado de Docstring, pode ser utilizado em mais de uma linha.
+Por exemplo:
+"Minha terra tem palmeiras,
+Onde canta o Sabiá;
+As aves, que aqui gorjeiam,
+Não gorjeiam como lá. '''
+#Já este comentário é somente para uma linha
+
+print("Hello World") #este comentário exibe na tela. É dado de saída
