80ebcf24714d4b3edf992ff10150ed3997c1d733
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-24 20:28:17 -0300

    Create aula5.py
    Nesta aula, você vai aprender que existe um tipo de dado chamado Boolean.
    No Python é bool.
    Ele serve para responder se o argumento é verdadeiro (True) ou falso (False),

---
+++ aula5.py
@@ -0,0 +1,16 @@
+# Tipo de dado bool (boolean)
+# Somente existem duas respostas possíveis:
+# verdadeiro (True) ou falso (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o == (igual), != (diferente), >, >=, <, <=.
+print(2025 == 2025)  # Sim => True (Verdadeiro)
+print(2025 == 2520)  # Não => False (Falso)
+print(type(True))
+print(type(False)) #bool
+print(type(1001 != 1001)) #bool
+print(type(10 > 11)) #bool
+print(type(10 < 11)) #bool
+print(type(10 <= 11)) #bool
+print(10 > 11) #False
+print(10 < 11) #True
+print(10 <= 11) #True
