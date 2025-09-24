112b640959db7462251719c5060e8259648a0d16
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-24 19:57:23 -0300

    Create aula4.py
    Nessa aula você vai aprender que os números podem ser int (inteiro) ou float (com casas decimais).
    
    No final do código mostra o comando type entre parênteses do print. Ele exibe no terminal a classe se aquele dado que estiver dentro dos parênteses é string, int, float ou bool.
    
    O bool será estudado mais a frente, mas só para dar um pequeno spoiler, o bool são variáveis que podem ser true(verdadeiro) ou false(falso), não se atente nisso agora.

---
+++ aula4.py
@@ -0,0 +1,18 @@
+# Tipos int e float (são variáveis)
+# int -> Número inteiro Ex. 10
+# O tipo int representa qualquer número inteiro, positivo(sem sinal de +) ou negativo
+print(54) # int
+print(-54) # int
+print(0) #int
+
+# float -> Número com ponto flutuante, ou seja, são números com casas decimais
+# O tipo float representa qualquer número positivo ou negativo com casas decimais
+print(5.5, 47.98) #float
+print(0.0, -17.48) #float
+
+#OBS.: Se o número tem casa decimal é flot, se não tem casa decimal é int.
+
+# A função type mostra o tipo da variável que o Python inferiu ao valor.
+print(type('Hildebrando'))
+print(type(0), type('Python'), type(True))
+print(type(5.5), type(-17.48), type(0.0))
