03ecbdadb4a01d28e15948cd964b89ae0c2999ef
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-25 20:07:44 -0300

    Create aula9.py
    Nessa aula você vai aprender sobre operadores aritméticos como: adição, subtração, multiplicação, divisão, divisão inteira, exponenciação e módulo.

---
+++ aula9.py
@@ -0,0 +1,28 @@
+#Operadores Aritiméticos
+
+adicao = 25 + 25
+print('Adição:', adicao)
+
+subtracao = 18 - 7
+print('Subtração:', subtracao)
+
+multiplicacao = 9 * 10
+print('Multiplicação:', multiplicacao)
+
+divisao = 25 / 3  # nesse caso o resultado será em float
+print('Divisão:', divisao)
+
+divisao_inteira = 25 // 3 #nesse caso o resultado será sempre inteiro, mesmo que a resposta seja 3.333 (neste caso)
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 2 ** 9
+print('Exponenciação:', exponenciacao)
+
+modulo = 128 % 3  # a resposta será o resto da divisão
+print('Módulo:', modulo)
+
+print(10 % 2 == 0)
+print(24 % 9 == 0)
+print(49 % 7 == 0)
+print(25 % 5 == 1)
+print(81 % 4 == 0)
