f99d29cad4487e86ac5ec9d8520f84887f941a92
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-25 20:18:23 -0300

    Create aula10.py

---
+++ aula10.py
@@ -0,0 +1,11 @@
+#Concatenação(+) e Repetição(*) 
+
+concatenacao = 'Hildebrando' + ' ' + 'Marques' #concatenação
+#Hildebrando Marques
+print(concatenacao)
+
+sete_vezes = 'A' * 7 #AAAAAAA
+cinco_vezes_Brasil = 5 * 'Brasil' #repetição
+#BrasilBrasilBrasil 
+print(sete_vezes)
+print(cinco_vezes_Brasil)
