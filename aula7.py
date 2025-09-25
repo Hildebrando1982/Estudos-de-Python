626e0ce225614fa999626b5d0c1e16ce26378f49
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-25 18:18:23 -0300

    Create aula7.py
    Nessa aula vamos aprender o que são variáveis.
    Variáveis são dados de entrada que deve ser usadas para salvar algo na memória do computador.
    PEP8 (www.pep8.org): inicie as variáveis com letras minúsculas, pode usar números e underline _ (SnakeCase).
    Em uma variável é possível usar o sinal de '=' como operador de atribuição (um valor a um nome de uma variável) e não igual em operação aritmética.

---
+++ aula7.py
@@ -0,0 +1,12 @@
+# Variáveis são dados de entrada que deve ser usadas para salvar algo na memória do computador.
+# PEP8: inicie as variáveis com letras minúsculas, pode usar números e underline _.
+# O sinal de '=' é usado o operador de atribuição e não igual em operação aritmética. 
+# O '=' é usado para atribuir um valor a um nome de uma variável.
+# Por exemplo: nome_variavel = argumento (Snake Case contem o _ separando as palavras)
+
+nome = 'Hildebrando Marques'
+idade = 25
+maior_de_idade = idade >= 18
+
+print('Nome:', nome, 'Idade:', idade) #Vai exibir na tela - Nome: Hildebrando Marques Idade: 25 
+print('É maior?', maior_de_idade) #Vai exibir na tela - É maior? True
