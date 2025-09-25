4d29378ca940de31306d190f5dcfd5c3b23dd117
Hildebrando Filho <hmarques1982@gmail.com>
2025-09-25 18:39:22 -0300

    Create aula9-exercício.py
    Exercícios sobre atribuir variáveis.

---
+++ aula9-exercício.py
@@ -0,0 +1,56 @@
+'''
+Exercício de fixação
+Observe que não foram atribuidas as variáveis e caso coloque o programa para rodar, dará uma mensagem de erro.
+Atribua as variáveis para o programa reconhecê-lo, eliminando assim, a mensagem de erro
+A resposta está na linha 50
+'''
+
+print('Nome', nome)
+print('Sobrenome', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+nome = 'Hildebrando'
+sobrenome = 'Marques'
+idade = 43
+ano_nascimento = 1982
+maior_idade = idade >= 18
+altura_metros = 1.78
+'''
