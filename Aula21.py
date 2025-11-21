56fe15f0c31e687da545498ef649caf368011818
Hildebrando Filho <hmarques1982@gmail.com>
2025-11-21 12:56:17 -0300

    Enhance comments and add logical operator examples
    Added comments explaining the 'not' operator and included examples using 'and' and 'or'.

---
+++ Aula21.py
@@ -0,0 +1,14 @@
+# Operador lógico "not" (não)
+# Usado para inverter expressões, ou seja, se for verdadeiro, fica falso. 
+# not True = False
+# not False = True
+# senha = input('Senha: ')
+print(not True)  # False
+print(not False)  # True
+
+
+#Usando and e or para revisar
+print(1 and 0)
+print(1 or 0)
+print(1 or 0 and 1 and 1)
+print(0 or 1 and 0 and 1)
