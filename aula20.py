ac63f797d45ed5c67ed1446e49a1a3d2d0017b08
Hildebrando Filho <hmarques1982@gmail.com>
2026-03-11 21:46:03 -0300

    Update aula20.py

--- aula20.py
+++ aula20.py
@@ -8,15 +8,24 @@
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
-entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Senha: ')
 
-senha_permitida = '123456'
-
-if entrada == 'E' and senha_digitada == senha_permitida:
-     print('Entrar')
+entrada = input('Digite [E]ntrar ou [S]air: ')
+    
+if entrada == "E".lower():
+    senha_digitada = input('Digite a senha: ')
+    senha_entrada = '123456'
+    if senha_digitada == senha_entrada:
+        print('Acesso Permitido!!!')
+    elif not senha_digitada:
+        print('Você não digitou nada.')
+        print('Tente novamente.')
+    else:
+        print('Acesso negado!')
+elif entrada == "S".lower():
+    print("Você optou em sair do sistema.")
 else:
-     print('Sair')
+    print("Você digitou um caractere incorreto.")
+    print("Tente novamente.")  
 
 # Avaliação de curto circuito
 print(True and False and True)
