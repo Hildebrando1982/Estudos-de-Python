ef5e40f6b8ef875e34285a0318cf53ce8449a1af
Hildebrando Filho <hmarques1982@gmail.com>
2026-03-20 14:51:18 -0300

    Exercício guiado com while

---
+++ aula44.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""
+#       012345678910
+nome = 'Hildebrando Filho'  # Iteráveis
+#      1110987654321
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+nova_string = ''
+nova_string += '*H*i*l*d*e*b*r*a*n*d*o* *F*i*l*h*o'
