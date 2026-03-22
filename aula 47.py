1cdcba4a04c417b0916c30cfa496d33257eaec29
Hildebrando Filho <hmarques1982@gmail.com>
2026-03-22 00:11:44 -0300

    While / else (Recurso Peculiar do Python)

---
+++ aula 47.py
@@ -0,0 +1,15 @@
+'''While / else'''
+
+string = 'Ordem e Progresso'
+
+i = 0
+
+while i < len(string):
+    letra = string[i]
+    if letra == ' ':
+        break
+    print(letra)
+    i += 1
+else:
+    print('O else foi executado')
+print('Fora do while')
